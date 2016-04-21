497aa45b3098f2d6aeacf7e365a46e28818deaa5
Will Richardson <willr@catalyst.net.nz>
2016-04-22 11:55:32 +1200

    Issues can be dragged from column to column

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -1,5 +1,23 @@
 (function() {
+  function moveIssue(issueElem, target) {
+    var issue = $(issueElem);
+    var column = $(target);
+    issue.remove();
+    issue.css({ position: '', left: '', top: '' });
+    column.append(issue);
+  }
+
   $(document).ready(function() {
-    $('.draggable').draggable();
+    $('.draggable').draggable({
+      containment: '#kanban-container',
+      cursor: 'move',
+      snap: '#kanban-container'
+    });
+
+    $('.droppable').droppable({
+      drop: function(ev, ui) {
+        moveIssue(ui.draggable, ev.target);
+      }
+    })
   });
 })();
