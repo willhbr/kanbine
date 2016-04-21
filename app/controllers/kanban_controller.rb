025cc1ded92f747f94d72fb633da7a60ecd01ac0
Will Richardson <willr@catalyst.net.nz>
2016-04-22 10:50:15 +1200

    Menu in the menubar and silly permission

---
+++ app/controllers/kanban_controller.rb
@@ -0,0 +1,5 @@
+class KanbanController < ApplicationController
+  def show
+
+  end
+end
--- init.rb
+++ init.rb
@@ -1,12 +1,25 @@
+# Automatically load all ruby files in lib/ with 'kanbine' prefix
 Dir[File.expand_path(File.dirname(__FILE__) + '/lib/kanbine_*.rb')].each do |file|
   require_dependency file.split('/').last.chomp('.rb')
 end
 
 Redmine::Plugin.register :kanbine do
-  name 'Kanbine plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  name 'Kanbine'
+  author 'Will Richardson'
+  description 'Kanban board for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author_url 'http://javanut.net'
+
+  permission :kanban_it_up, {
+    kanban: [
+      :show
+    ]
+  }
+
+  menu :project_menu, :kanbine_kanban, {
+    controller: :kanban,
+    action: :show
+  }, caption: :label_kanban, after: :roadmap
+
 end
