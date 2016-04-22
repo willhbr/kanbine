d3ceb53a172d419b5ed690dc9ae25fee6745fc98
Will Richardson <willr@catalyst.net.nz>
2016-04-22 13:10:26 +1200

    Removed everything and replaced it with sortable

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -1,33 +1,8 @@
 (function() {
-  function bindDraggable(domElems) {
-    domElems.draggable({
-      containment: '#kanban-container',
-      cursor: 'move',
-      snap: '#kanban-container',
-      revert: 'invalid'
-    });
-  }
-
-  function moveIssue(issueElem, target) {
-    var issue = $(issueElem);
-    var column = $(target);
-    issue.remove();
-    issue.css({
-      position: 'relative',
-      left: '',
-      top: ''
-    })
-    column.append(issue);
-  }
-
   $(document).ready(function() {
-    bindDraggable($('.draggable'));
-
-    $('.droppable').droppable({
-      drop: function(ev, ui) {
-        moveIssue(ui.draggable, ev.target);
-      },
-      hoverClass: 'hovering'
+    $('#kanban-container').find('.kanban-column .sortable').sortable({
+      connectWith: '.kanban-column .sortable',
+      revert: true
     });
   });
 })();
