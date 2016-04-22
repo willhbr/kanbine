e2cf710f2b868562039faf8406abcb96c0023b58
Will Richardson <willr@catalyst.net.nz>
2016-04-22 13:59:52 +1200

    Hover indicater is styleable, removed column colouring

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -2,10 +2,13 @@
   $(document).ready(function() {
     $('#kanban-container').find('.kanban-column .sortable').sortable({
       connectWith: '.kanban-column .sortable',
-      revert: true,
-      over: function(ev, ui) {
-        $('.kanban-column .sortable').removeClass('hovering');
-        $(this).addClass('hovering');
+      revert: 100,
+      placeholder: 'placeholder',
+      start: function(event, ui) {
+        ui.placeholder.height(ui.helper.height())
+      },
+      stop: function(ev, ui) {
+        // stopped
       }
     });
   });
