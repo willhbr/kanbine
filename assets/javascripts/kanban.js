f85aacaa92b64ee3a18bf3913fb4682277540097
Will Richardson <willr@catalyst.net.nz>
2016-04-26 14:25:17 +1200

    Initial structure for update dialog

--- assets/javascripts/kanban.js
+++ assets/javascripts/kanban.js
@@ -1,4 +1,21 @@
 (function() {
+  function showDialogFor(elem) {
+    var html = $('#dialog-helper').html();
+    var title = 'DIALOG  TITLE WOOOO';
+    $('<div></div>').appendTo('body').html(html).dialog({
+      modal: true, title: title, autoOpen: true,
+      width: 300, resizable: true,
+      buttons: {
+        'Save': function(){},
+        'Cancel': function (event, ui) {
+            $(this).dialog('close');
+        }
+      },
+      close: function (event, ui) {
+          $(this).remove();
+      }
+    });
+  }
   function issueToJSON(elem) {
     var data = {
       id: elem.data('id'),
@@ -18,6 +35,7 @@
       if(res.saved) {
         console.log("Saved!");
       } else {
+        elem.addClass('ajax-error');
         alert(res.errors);
       }
     }).fail(function(res) {
@@ -25,17 +43,26 @@
     }).always(function(res) {
     });
   }
-
+  var allowClick = true;
   $(document).ready(function() {
     $('#kanban-container').find('.kanban-column .sortable').sortable({
       connectWith: '.kanban-column .sortable',
       revert: 100,
       placeholder: 'placeholder',
+      distance: 10,
       start: function(event, ui) {
+        allowClick = false;
         ui.placeholder.height(ui.helper.height())
       },
       stop: function(ev, ui) {
+        allowClick = true;
         updateStatusPosition(ui.item);
+      }
+    });
+    $('.kanban-row .issue-subject').on('click', function(e) {
+      if(allowClick) {
+        console.log(e);
+        showDialogFor($(e.currentTarget).parent());
       }
     });
   });
