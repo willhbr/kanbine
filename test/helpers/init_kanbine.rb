8ae75d9de48ca3ef4700bce635a697e57f71c11d
Will Richardson <willr@catalyst.net.nz>
2016-04-27 14:31:11 +1200

    Fixed tests with proper initialization

--- test/helpers/init_kanbine.rb
+++ test/helpers/init_kanbine.rb
@@ -1,5 +1,7 @@
 module Helpers
   def init_kanbine(proj)
     proj.kanban_statuses = IssueStatus.where.not(name: ['Feedback', 'Rejected', 'Resolved']).all
+    proj.kanban_trackers = Tracker.all
+    proj.enable_module! 'kanbine'
   end
 end
